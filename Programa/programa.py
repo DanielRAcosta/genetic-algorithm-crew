e32664d093303dc8dbcd693cb5dea04e5f3f86e2
Daniel Rodrigues Acosta <daniel.acosta@ufrgs.br>
2019-11-05 21:18:27 -0300

    Pop e Mutação - Elizondo
    Nomes das populações de acordo com Elizondo; Mutações substituem pais e usam porcentagem Random definida em VarGlobais

--- Programa/programa.py
+++ Programa/programa.py
@@ -26,50 +26,56 @@ for i in range(gl.na): popA.addSol(sl.Solucao(sv.Servico(rd.randrange(1,len(gl.v
 # INICIA O LAÇO PRINCIPAL #######################################################
 
 for iAlg in range(gl.alg):
-    popB.sols.clear() #esvazia B para recomeçar
+    popC.sols.clear() #esvazia B para recomeçar
     
     ### CRUZAMENTO ############
     # pra cada um dos nb melhores integrantes de A, de acordo com a função seleçãoDeterminística 
-    for iSol in popA.selecDet(gl.nb,[]):  # CRUZAMENTO B->C - repetir até gerar nCruz filhos cruzados dos pais de A
+    for iSol in popA.selecDet(round(gl.fs*gl.na),[]):  # CRUZAMENTO B->C - repetir até gerar nCruz filhos cruzados dos pais de A
         # os pais dos cruzamentos aqui são buscados diretamente em A.
-        for jSol in range(gl.nCruz): # cruza com vdict
+        for jSol in range(gl.nCruz): # cruza com E (vdict)
             ex = sl.Solucao(sv.Servico(popA.sols[iSol].viagNovaRandom())) # cria solução com viagem que ainda não existe em B
-            popB.addSol(popA.sols[iSol].cruza(ex.viagSol)) #filho BE
-            popB.addSol(ex.cruza(popA.sols[iSol].viagSol)) #filho EB
+            popC.addSol(popA.sols[iSol].cruza(ex.viagSol)) #filho CE
+            popC.addSol(ex.cruza(popA.sols[iSol].viagSol)) #filho EC
         
         for jSol in popA.selecRoleta(gl.nCruz,[]): #cruza com A
-            popB.addSol(popA.sols[iSol].cruza(popA.sols[jSol].viagSol)) #filho BA
-            popB.addSol(popA.sols[jSol].cruza(popA.sols[iSol].viagSol)) #filho AB
+            popC.addSol(popA.sols[iSol].cruza(popA.sols[jSol].viagSol)) #filho CA
+            popC.addSol(popA.sols[jSol].cruza(popA.sols[iSol].viagSol)) #filho AC
         
-        if iAlg>0: #cruza com C
-            for jSol in popC.selecRoleta(gl.nCruz,[]): 
-                popB.addSol(popA.sols[iSol].cruza(popC.sols[jSol].viagSol)) #filho BD
-                popB.addSol(popC.sols[jSol].cruza(popA.sols[iSol].viagSol)) #filho DB
+        if iAlg>0: #cruza com B
+            for jSol in popB.selecRoleta(gl.nCruz,[]): 
+                popC.addSol(popA.sols[iSol].cruza(popB.sols[jSol].viagSol)) #filho CB
+                popC.addSol(popB.sols[jSol].cruza(popA.sols[iSol].viagSol)) #filho BC
                 
-        popB.addSol(popA.sols[iSol]) #só no final os pais advindos de A são adicionados à população, para que também sejam passíveis de mutação na próxima etapa
+        popC.addSol(popA.sols[iSol]) #só no final os pais advindos de A são adicionados à população, para que também sejam passíveis de mutação na próxima etapa
     
     ### MUTAÇÃO ############
-    mutarSolucoes = popB.selecRand(gl.nMut, []) #quais soluções de B serão mutadas? deve ser declarado aqui para que o laço abaixo não gere soluções duplamente mutadas
-    for iSol in mutarSolucoes: popB.addSol(popB.sols[iSol].muta()) # adiciona à popB os filhos dos pais escolhidos acima 
+    # antigo:
+    #mutarSolucoes = popC.selecRand(gl.nMut, []) #quais soluções de B serão mutadas? deve ser declarado aqui para que o laço abaixo não gere soluções duplamente mutadas
+    #for iSol in mutarSolucoes: popC.addSol(popC.sols[iSol].muta()) # adiciona à popB os filhos dos pais escolhidos acima 
+    
+    # novo: mutação substitui
+    for iSol in popC.sols:
+        if rd.random() < gl.pm: popC.sols[iSol].muta()
     
     # só para checar:
-    idsolsB = [popB.sols[key].idsol for key in popB.sols]
+    idsolsC = [popC.sols[key].idsol for key in popC.sols]
     
     ### SELEÇÃO ############
     
-    # deterministica B --> C
-    idsolsC = [popC.sols[key].idsol for key in popC.sols] # lista de idsols das soluções que existem em C. deve entrar como argumento na função abaixo para evitar duplicatas
-    melhores = popB.selecDet(gl.nTop,idsolsC) # retorna iterador com as keys das soluções escolhidas em B
-    for iSol in melhores: popC.addSol(popB.sols[iSol]) # adiciona à população C as soluções escolhidas acima
+    # deterministica C --> B
+    idsolsB = [popB.sols[key].idsol for key in popB.sols] # lista de idsols das soluções que existem em C. deve entrar como argumento na função abaixo para evitar duplicatas
+    melhores = popC.selecDet(gl.nTop,idsolsB) # retorna iterador com as keys das soluções escolhidas em B
+    for iSol in melhores: popB.addSol(popC.sols[iSol]) # adiciona à população C as soluções escolhidas acima
+    """ será que aqui a declaração está deletando algo? acho que não."""
     
-    # roleta B --> A
+    # roleta C --> A
     idsolsA = [popA.sols[key].idsol for key in popA.sols] # lista de idsols das soluções que existem em A. deve entrar como argumento na função abaixo para evitar duplicatas
-    roleta = popB.selecRoleta(gl.nRol,idsolsA) # retorna iterador com as keys das soluções escolhidas em B
-    for iSol in roleta: popA.addSol(popB.sols[iSol]) # adiciona à população A as soluções escolhidas acima
+    roleta = popC.selecRoleta(gl.nRol,idsolsA) # retorna iterador com as keys das soluções escolhidas em B
+    for iSol in roleta: popA.addSol(popC.sols[iSol]) # adiciona à população A as soluções escolhidas acima
     
-    # cortar as piores dos grupos constantes A & C
+    # cortar deterministicamente as piores dos grupos constantes A & B
     popA.excluiDet()
-    popC.excluiDet()
+    popB.excluiDet()
 
     # plota no arquivo output a cada iteração
     for sol in popC.sols:
@@ -77,7 +83,8 @@ for iAlg in range(gl.alg):
         f.write(out)
     
     # plota na tela o número máximo de viagens
-    print(iAlg, popA.maxViagSol(), popB.maxViagSol(), popC.maxViagSol())
+    print(iAlg, popA.sizeViagSol(), popB.sizeViagSol(), popC.sizeViagSol())
+    
     
 #Fim do laço   
 #%%
